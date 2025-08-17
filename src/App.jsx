b4adde7a0b61163c45fad2c3d638c26c5c6c2fcf
rahamath45 <rahmathsulaiman45@gmail.com>
2025-08-17 18:18:54 +0530

    the search page

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
 
-
+import LiveTvIcon from '@mui/icons-material/LiveTv';
 import './App.css'
+import { Link, Route, Routes } from 'react-router-dom'
+import Search from './pages/Search';
+import Details from './pages/Details';
 
 function App() {
  
 
   return (
     <>
-      
+      <nav className='app'>
+        <LiveTvIcon></LiveTvIcon>
+           <Link to="/" className='link'>OMDb Movie
+           </Link>
+      </nav>
+          <Routes>
+            <Route exact path="/" element={<Search/>} />
+            <Route path="/movie/:id" element={<Details/>} />
+          </Routes>
     </>
   )
 }
